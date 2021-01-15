2ab75a22d063cae8a1e2c1d46901fd8057b31eeb
megan gosnell <megan_gosnell@aol.co.uk>
2021-01-15 14:44:48 +0000

    first step of 15 complete

--- lib/journey_log.rb
+++ lib/journey_log.rb
@@ -1,3 +1,13 @@
 class JourneyLog
-
+    attr_reader :journey_class
+    def initialize(journey_class: Journey)
+        @journey_class = journey_class 
+        @journeys = []
+    end
+def start_journey(station) 
+    add(journey_class.new(entry_station: station))
+end
+def add(journey)
+    @journeys << journey
+  end
 end 
--- spec/journey_log_spec.rb
+++ spec/journey_log_spec.rb
@@ -1,8 +1,14 @@
 require 'journey_log'
-
 describe JourneyLog do
+let(:journey){ double :journey, entry_station: nil, complete?: false, fare: 1}
+let(:station){ double :station}
+let(:journey_class) {double :journey_class, new: journey}
+subject(:journey_log){described_class.new(journey_class: journey_class)}
 
-  
-
-
+describe "#start_journey" do 
+    it 'starts a journey' do 
+      expect(journey_class).to receive(:new).with(entry_station: station)
+      journey_log.start_journey(station)
+    end
 end
+  end
