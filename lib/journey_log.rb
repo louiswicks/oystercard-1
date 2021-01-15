2fe9ae570e21a03999cc8441c3dc153edd82c107
Louis Wickremeratne <louiswickremeratne@yahoo.co.uk>
2021-01-15 15:01:47 +0000

    Another part of Step 15 completed

--- lib/journey_log.rb
+++ lib/journey_log.rb
@@ -1,13 +1,20 @@
 class JourneyLog
     attr_reader :journey_class
     def initialize(journey_class: Journey)
-        @journey_class = journey_class 
+        @journey_class = journey_class
         @journeys = []
     end
-def start_journey(station) 
+
+  def start_journey(station)
     add(journey_class.new(entry_station: station))
-end
-def add(journey)
+  end
+
+  def add(journey)
     @journeys << journey
   end
-end 
+
+  def journeys
+    @journeys.dup
+  end
+
+end
--- spec/journey_log_spec.rb
+++ spec/journey_log_spec.rb
@@ -1,14 +1,19 @@
 require 'journey_log'
 describe JourneyLog do
-let(:journey){ double :journey, entry_station: nil, complete?: false, fare: 1}
-let(:station){ double :station}
-let(:journey_class) {double :journey_class, new: journey}
-subject(:journey_log){described_class.new(journey_class: journey_class)}
+  let(:journey){ double :journey, entry_station: nil, complete?: false, fare: 1}
+  let(:station){ double :station}
+  let(:journey_class) {double :journey_class, new: journey}
+   subject(:journey_log){described_class.new(journey_class: journey_class)}
 
 describe "#start_journey" do 
-    it 'starts a journey' do 
+    it 'starts a journey' do
       expect(journey_class).to receive(:new).with(entry_station: station)
       journey_log.start_journey(station)
     end
-end
+
+    it 'records a journey' do
+      journey_log.start_journey(station)
+      expect(journey_log.journeys).to include(journey)
+    end
   end
+end
